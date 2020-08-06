79bb1ae288039594683cee8ef0135a4c255cf2fa
darintrimillos <darin@trimillos.com>
2020-08-05 18:12:21 -0700

    add form, submit button

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("submitted");
+  }
   
   return (
     <div className="App">
-      <label htmlFor="name">Home:</label>
-      <input
-        type="text"
-        id="name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        aria-label="Name"
-        aria-required="true"
-        required
-      />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Home:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          aria-label="Name"
+          aria-required="true"
+          required
+        />
+
+        <input className="submit" type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
